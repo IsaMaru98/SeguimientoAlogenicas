74e406c653b0c545dd0da634fbd881799ecd030d
[Isa Maru] <mimarulandaa@unal.edu.co>
2023-02-15 14:20:02 -0500

    enviando datos del modelo al template por medio de la vista

--- formularios/views.py
+++ formularios/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, HttpResponse
 
-
+from .models import Cosecha
 # Create your views here.
 
+## en esta función llamo los datos de Cosecha con el objects.all() y estos los paso por medio de un diccionario al html 
+# en el archivo html puedo mostrar estos datos del objeto usando un ciclo for 
+
 def home(request): 
-    return render(request, 'formularios/home.html', {} )
+    datosCosecha = Cosecha.objects.all()
+    return render(request, 'formularios/home.html', {'datosCosecha': datosCosecha} )
