15a60992d7558304b9900bc2b022f263faab0a28
[Isa Maru] <mimarulandaa@unal.edu.co>
2023-02-15 12:38:58 -0500

    se crean los modelos Cosecha, Siembra, Crio

--- formularios/admin.py
+++ formularios/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from . import models 
+
+
 # Register your models here.
+
+## Este objeto hace que en la consola de administrador no me pida una clave primaria ya que la estoy autogenerando con la función definida en models.py
+class CosechaAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        if obj is None:
+            # Si el objeto es nuevo, usa un formulario sin la clave primaria
+            self.exclude = ('idCosecha',)
+        else:
+            # Si el objeto ya existe, usa el formulario por defecto
+            self.exclude = ()
+        return super(CosechaAdmin, self).get_form(request, obj, **kwargs)
+
+admin.site.register(models.Cosecha, CosechaAdmin)
+admin.site.register(models.Siembra)
+admin.site.register(models.Crio)
--- formularios/models.py
+++ formularios/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Cosecha(models.Model):
+    loteCosecha = models.CharField(default="", max_length=50)
+    fechaCosecha = models.DateField()
+    paseCosecha = models.CharField(max_length=50)
+    areaFrascosCosecha = models.IntegerField(choices=[(25, 'T25'), (75, 'T75'), (175, 'T175'), (850, 'CR Liso'), (2125, 'CR Corrugado'), (0, 'Crioviales')]) 
+    numFrascosCosecha = models.IntegerField()
+    numCelulasObtenidas = models.FloatField()
+    viabilidadCosecha = models.IntegerField()
+    idCosecha = models.CharField(max_length=50, unique=True, primary_key=True)
+
+    def save(self, *args, **kwargs): 
+        self.idCosecha = self.generarIdCosecha()
+        super().save(*args,*kwargs)
+
+    def generarIdCosecha(self):
+        return f"{self.fechaCosecha}-{self.areaFrascosCosecha}-{self.numFrascosCosecha}-{self.loteCosecha}"
+
+class Siembra(models.Model): 
+    loteSimbra = models.TextField() 
+    fechaSiembra = models.DateField()
+    paseSiembra = models.TextField()
+    areaFrascosSiembra = models.IntegerField(choices=[(25, 'T25'), (75, 'T75'), (175, 'T175'), (850, 'CR Liso'), (2125, 'CR Corrugado'), (0, 'Crioviales')])
+    numFrascosSiembra = models.IntegerField()
+    numCelulasSembradasXFrasco = models.FloatField()
+    cosechaFK = models.ForeignKey(Cosecha, on_delete=models.CASCADE)
+    
+     
+class Crio(models.Model): 
+    numCelulasXVial = models.FloatField()
+    numViales = models.IntegerField() 
+
+
