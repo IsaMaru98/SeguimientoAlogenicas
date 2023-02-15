c89a8df5a3695e67e1c7c647ee5577c76d7deb02
[Isa Maru] <mimarulandaa@unal.edu.co>
2023-02-15 13:11:06 -0500

    despliegue an pythonanywhere

--- Seguimiento_Alogenicas/settings.py
+++ Seguimiento_Alogenicas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dfe1io$+9&gdo&&y#xppl^)y-p#3qv5p6nwq@4%4!(@#@klson
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-419'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
