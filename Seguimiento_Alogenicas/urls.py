37ef2cee41a1acd8e276bd8ee488b68162494da0
Isamaru <mimarulandaa@unal.edu.co>
2023-07-18 14:30:37 -0500

    cambios menores en la tabla de datos, configuración inicial de api

--- Seguimiento_Alogenicas/urls.py
+++ Seguimiento_Alogenicas/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include 
+from formularios.api import api 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('formularios.urls')),
+    path('api/', api.urls), 
 
 ]
--- formularios/urls.py
+++ formularios/urls.py
@@ -1,13 +1,5 @@
 from django.urls import path 
 from . import views 
-from ninja import NinjaAPI
-
-api = NinjaAPI()
-
-
-@api.get("/add")
-def add(request, a: int, b: int):
-    return {"result": a + b}
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -17,5 +9,4 @@ urlpatterns = [
     path('crio/',views.crio, name='formularioCrio'),
     path('formulario_datos/',views.datoForm, name="formularioDato"), 
     path('lotes/', views.lotes, name='lotes'),
-    path("api/", api.urls),
 ]
