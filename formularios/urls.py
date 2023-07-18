a4bacdf83b67c10ce3be8ee3442855657b019962
Isamaru <mimarulandaa@unal.edu.co>
2023-07-18 07:19:11 -0500

    api

--- formularios/urls.py
+++ formularios/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path 
 from . import views 
+from ninja import NinjaAPI
+
+api = NinjaAPI()
+
+
+@api.get("/add")
+def add(request, a: int, b: int):
+    return {"result": a + b}
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,5 +16,6 @@ urlpatterns = [
     path('siembra/', views.siembra, name='formularioSiembra'), 
     path('crio/',views.crio, name='formularioCrio'),
     path('formulario_datos/',views.datoForm, name="formularioDato"), 
-    path('lotes/', views.lotes, name='lotes')
+    path('lotes/', views.lotes, name='lotes'),
+    path("api/", api.urls),
 ]
